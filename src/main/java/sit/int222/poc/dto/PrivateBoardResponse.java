b76dcb4bc7245ee8b6fb2eefae29b25389d3329c
ChinnaphatLoha <chinnaphat.loha@mail.kmutt.ac.th>
2024-08-08 19:32:51 +0700

    feat: Add PrivateBoardResponse DTO class
    - Added a new DTO class called PrivateBoardResponse to represent the response data for private boards.
    - The class includes fields for id, title, description, isPublic, and owner.
    - This DTO will be used to send private board data to clients in a secure manner.

---
+++ src/main/java/sit/int222/poc/dto/PrivateBoardResponse.java
@@ -0,0 +1,14 @@
+package sit.int222.poc.dto;
+
+import lombok.Data;
+import sit.int222.poc.user_account.User;
+
+@Data
+public class PrivateBoardResponse {
+    private Long id;
+    private String title;
+    private String description;
+    private Boolean isPublic;
+    private User owner;
+
+}
